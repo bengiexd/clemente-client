bbbf6a6217b1d39b828b7b5c9c868914c3d567bd
Bengie Nick <bengiexd@gmail.com>
2013-10-03 19:33:55 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  main.py
+#  
+#  Copyright 2013 valentin basel <valentin@gmail.com>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+import socket
+import sys
+import interface
+
+class Main():
+	""" ClassName  Main
+    	Clase cliente para interactuar con el servidorjango
+	"""
+
+def HELP():
+	print "Ayuda para entrar a ClementeCliente;"
+def VERSION():
+	print "version de ClementeCliente 1.0"
+def IP():
+	pass	
+def main():
+	arg={"-h": HELP,"-v":VERSION, "-ip": IP}
+	if len(sys.argv)>1:
+		for dat in sys.argv[1:]:
+			if arg.has_key(dat):
+				arg[dat]()
+			else:
+				print "No se reconoce el comando ", dat
+				exit()
+	cliente = interface.Controller("127.0.0.1",8002)
+
+	return 0
+
+if __name__ == '__main__':
+	main()
+
